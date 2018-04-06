b4b1128eaee707ea9497b2e0fec9f3813e05fd98
Marc Laliberté <laliberte.marc1@gmail.com>
2018-04-05 20:58:57 -0400

    added more queries

--- Jikiki/script.js
+++ Jikiki/script.js
@@ -1,12 +1,69 @@
 
 window.onload=function(){
+
+}
+
+function queryClients(){
     $.ajax({
         type: 'POST',
-        url: '/displayClients',
+        url: '/displayclient',
         data: {},
         success: function(result) {
             var array=JSON.parse(result)
-          displayAllClients(array["result"])
+            displayAllClients(array["result"])
+        },
+    });
+}
+
+function queryItems(){
+    $.ajax({
+        type: 'POST',
+        url: '/displayitems',
+        data: {},
+        success: function(result) {
+            var array=JSON.parse(result)
+
+            displayAllItems(array["result"])
+        },
+    });
+}
+
+function queryWeapons(){
+    $.ajax({
+        type: 'POST',
+        url: '/displayweapons',
+        data: {},
+        success: function(result) {
+            var array=JSON.parse(result)
+
+            displayAllWeapons(array["result"])
+        },
+    });
+}
+
+function queryArmors(){
+    $.ajax({
+        type: 'POST',
+        url: '/displayarmors',
+        data: {},
+        success: function(result) {
+            var array=JSON.parse(result)
+
+            displayAllArmors(array["result"])
+        },
+    });
+}
+
+
+function queryAnimals(){
+    $.ajax({
+        type: 'POST',
+        url: '/displayanimals',
+        data: {},
+        success: function(result) {
+            var array=JSON.parse(result)
+
+            displayAllAnimals(array["result"])
         },
     });
 }
@@ -23,4 +80,49 @@ function displayAllClients(array){
     $('div.mainTable').html(result);
 }
 
+function displayAllWeapons(array){
 
+    var result="<table><tr><th>ID</th><th>Name</th><th>Material</th></tr>";
+
+    for(var i=0;i<array.length;i++){
+        var row="<tr><td>"+array[i].id+"</td><td>"+array[i].name+"</td><td>"+array[i].material+"</td></tr>"
+        result+=row;
+    }
+    result+="</table>";
+    $('div.mainTable').html(result);
+}
+
+function displayAllItems(array){
+    var result="<table><tr><th>ID</th><th>Name</th></tr>";
+    for(var i=0;i<array.length;i++){
+        var row="<tr><td>"+array[i].id+"</td><td>"+array[i].name+"</td></tr>"
+        result+=row;
+    }
+    result+="</table>";
+    $('div.mainTable').html(result);
+}
+
+
+function displayAllArmors(array){
+
+    var result="<table><tr><th>ID</th><th>Name</th><th>Type</th><th>Material</th></tr>";
+
+    for(var i=0;i<array.length;i++){
+        var row="<tr><td>"+array[i].id+"</td><td>"+array[i].name+"</td><td>"+array[i].type+"</td><td>"+array[i].material+"</td></tr>";
+        result+=row;
+    }
+    result+="</table>";
+    $('div.mainTable').html(result);
+}
+
+function displayAllAnimals(array){
+
+    var result="<table><tr><th>ID</th><th>Name</th><th>Type</th><th>Sexe</th></tr>";
+
+    for(var i=0;i<array.length;i++){
+        var row="<tr><td>"+array[i].id+"</td><td>"+array[i].name+"</td><td>"+array[i].type+"</td><td>"+array[i].sexe+"</td></tr>";
+        result+=row;
+    }
+    result+="</table>";
+    $('div.mainTable').html(result);
+}
--- Jikiki/server.js
+++ Jikiki/server.js
@@ -12,91 +12,76 @@ var body=[];
 
 
 const requestHandler = (request, response) => {
-  	//console.log(request.url);
+    //console.log(request.url);
 
- 	var uri = url.parse(request.url).pathname
+    var uri = url.parse(request.url).pathname
   , filename = path.join(process.cwd(), uri);
 
 
-  	if(request.method === 'POST'){
-  		body=[];
-		request.on('data', (chunk) => 
-  		{
-  			body.push(chunk);
-  			
-		}).on('end', () => 
-		{
-			/*
-	  		body = Buffer.concat(body).toString();
-	  		body=body.split("&");
-	  		var name = body[0].replace("name=","");
-	  		var plat = body[1].replace("platform=","");
-	  		//console.log("new body", body);
-	  		*/
-	  		if(request.url==='/displayClients')
-			{
-
-				pool.query('SELECT * from client', (err, res) => { 
-  					let result = res.rows; 	
-  					//console.log(res.rows)		        
-			        response.writeHead(200, {"Context-Type" : "application/json"});
-			        response.write(JSON.stringify({result}));
-			       	response.end();
-			       	if(err){
-			       		console.log(err);
-			       		let result="";
-			       		response.writeHead(200, {"Context-Type" : "application/json"});
-			        	response.write(JSON.stringify({result: result}));
-			       		response.end();
-			       	}
-				})
-	
-				
-			}
-			/*
-			if(request.url == '/saves')
-			{
-				//loading from json to minimise requests to stats api
-				console.log("loading from buffer");
-				var savePath='./saves/'+name+'.json'
-	    		var content = fs.readFileSync(savePath);
-	    		let result = JSON.parse(content);
-	       		response.writeHead(200, {"Context-Type" : "application/json"});
-	       		response.write(JSON.stringify(
-	       		{
-	       			result: result
-	      		}));
-	       		response.end();
-			}
-		  		*/
-		});
-			    
+    if(request.method === 'POST'){
+        body=[];
+        request.on('data', (chunk) => 
+        {
+            body.push(chunk);
+            
+        }).on('end', () => 
+        {
+            /*
+            body = Buffer.concat(body).toString();
+            body=body.split("&");
+            var name = body[0].replace("name=","");
+            var plat = body[1].replace("platform=","");
+            //console.log("new body", body);
+            */
+            if(request.url==='/displayclient' || request.url==='/displayitems'
+                || request.url==='/displayarmors' || request.url==='/displayweapons' || request.url==='/displayanimals')
+            {
+                var table=request.url.split("/display")[1];
+                console.log(table)
+                pool.query('SELECT * from '+table, (err, res) => { 
+                    let result = res.rows;  
+                    //console.log(res.rows)             
+                    response.writeHead(200, {"Context-Type" : "application/json"});
+                    response.write(JSON.stringify({result}));
+                    response.end();
+                    if(err){
+                        console.log(err);
+                        let result="";
+                        response.writeHead(200, {"Context-Type" : "application/json"});
+                        response.write(JSON.stringify({result: result}));
+                        response.end();
+                    }
+                })                  
+            }
+          
+        });
+                
   }
 
   else{
-   	fs.exists(filename, function(exists) {
+    fs.exists(filename, function(exists) {
     if(!exists) 
     {
-      	response.writeHead(404, {"Content-Type": "text/plain"});
-      	response.write("404 Not Found\n");
-      	response.end();
-      	return;
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not Found\n");
+        response.end();
+        return;
     }
 
     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
-      	if(err) 
-      	{        
-	        response.writeHead(500, {"Content-Type": "text/plain"});
-	        response.write(err + "\n");
-	        response.end();
-	        return;
-      	}
+        if(err) 
+        {        
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(err + "\n");
+            response.end();
+            return;
+        }
 
-      	response.writeHead(200);
-      	response.write(file, "binary");
-      	response.end();
+        response.writeHead(200);
+        response.write(file, "binary");
+        response.end();
     });
   });
  }
